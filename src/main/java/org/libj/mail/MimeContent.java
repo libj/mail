c8112ead3ee12d71710bb6f064d5ef8731889a42
Seva Safris <seva@safris.org>
2019-07-20 20:13:21 +0200

    Add clone/equals/hashCode

--- src/main/java/org/libj/mail/MimeContent.java
+++ src/main/java/org/libj/mail/MimeContent.java
@@ -16,11 +16,15 @@
 
 package org.libj.mail;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  * The {@code MimeContent} represents message content with an associated mime
  * type.
  */
-public class MimeContent {
+public class MimeContent implements Cloneable, Serializable {
+  private static final long serialVersionUID = -51968810703836463L;
   private final String content;
   private final String type;
 
@@ -30,15 +34,14 @@ public class MimeContent {
    *
    * @param content The content string.
    * @param type The mime type string.
+   * @throws NullPointerException If {@code content} or {@code type} is null.
    */
   public MimeContent(final String content, final String type) {
-    this.content = content;
-    this.type = type;
+    this.content = Objects.requireNonNull(content);
+    this.type = Objects.requireNonNull(type);
   }
 
   /**
-   * Returns the content string.
-   *
    * @return The content string.
    */
   public String getContent() {
@@ -46,11 +49,31 @@ public class MimeContent {
   }
 
   /**
-   * Returns the type string.
-   *
    * @return The type string.
    */
   public String getType() {
     return type;
+  }
+
+  @Override
+  public MimeContent clone() {
+    return new MimeContent(content, type);
+  }
+
+  @Override
+  public int hashCode() {
+    return content.hashCode() ^ type.hashCode();
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (obj == this)
+      return true;
+
+    if (!(obj instanceof MimeContent))
+      return false;
+
+    final MimeContent that = (MimeContent)obj;
+    return content.equals(that.content) && type.equals(that.type);
   }
 }
